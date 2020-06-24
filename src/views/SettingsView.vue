c8e1efece09d96be3211b8f976d9fc511fe12074
TPccHorizon <carlos.schweizer@hotmail.com>
2020-06-24 23:08:02 +0200

    read available languages from i18n

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -2,12 +2,12 @@
     <div>
         <h1>{{ $t("settings.title") }}</h1>
         <v-col>
-            Mail: {{settings.user.mail}}
+            {{ $t("settings.mail") }}: {{settings.user.mail}}
         </v-col>
         <v-col>
             Language:
             <select v-model="$i18n.locale" @change="changeLang($event)">
-                <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">
+                <option v-for="(lang, i) in getAvailableLangs" :key="`Lang${i}`" :value="lang">
                     {{ lang }}
                 </option>
             </select>
@@ -24,7 +24,6 @@
         name: "SettingsView",
         data () {
             return {
-                langs: ['en', 'de'],
                 settings: {
                     user: {
                         mail: null
@@ -38,6 +37,11 @@
             this.axios
                 .get('https://my-json-server.typicode.com/TPccHorizon/APIMock/user-settings')
                 .then(response => (this.settings = response.data))
+        },
+        computed: {
+            getAvailableLangs: function() {
+                return Object.keys(this.$i18n.messages);
+            }
         },
         methods: {
             changeLang(event) {
